b818353814acf5f51c66922bdd21433e0c9d908e
Enes Malik Turhan <emalikturhan@gmail.com>
2019-01-22 23:06:08 +0300

    settings ekranına time ve weather modulleri eklendi

--- phoneAndroidApp/app/src/main/java/com/example/panta/orangemirror/SettingsActivity.java
+++ phoneAndroidApp/app/src/main/java/com/example/panta/orangemirror/SettingsActivity.java
@@ -1,154 +1,163 @@
 package com.example.panta.orangemirror;
 
-import android.content.Context;
-        import android.content.Intent;
-        import android.content.pm.PackageManager;
-        import android.media.Ringtone;
-        import android.media.RingtoneManager;
-        import android.net.Uri;
-        import android.os.Build;
-        import android.os.Bundle;
-        import android.preference.EditTextPreference;
-        import android.preference.ListPreference;
-        import android.preference.Preference;
-        import android.preference.PreferenceFragment;
-        import android.preference.PreferenceManager;
-        import android.preference.RingtonePreference;
-        import android.text.TextUtils;
-        import android.view.MenuItem;
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.LinearLayout;
+import android.widget.Switch;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class SettingsActivity extends AppCompatPreferenceActivity {
-    private static final String TAG = SettingsActivity.class.getSimpleName();
+public class SettingsActivity extends Activity implements View.OnClickListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        setContentView(R.layout.activity_settings);
 
-        // load settings fragment
-        getFragmentManager().beginTransaction().replace(android.R.id.content, new MainPreferenceFragment()).commit();
-    }
+        Button timeDecrementBtn = (Button) findViewById(R.id.timeDecrementBtn);
+        Button timeIncrementBtn = (Button) findViewById(R.id.timeIncrementBtn);
 
-    public static class MainPreferenceFragment extends PreferenceFragment {
-        @Override
-        public void onCreate(final Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
-            addPreferencesFromResource(R.xml.pref_settings);
+        Button weatherDecrementBtn = (Button) findViewById(R.id.weatherDecrementBtn);
+        Button weatherIncrementBtn = (Button) findViewById(R.id.weatherIncrementBtn);
 
-            // gallery EditText change listener
-            bindPreferenceSummaryToValue(findPreference(getString(R.string.key_gallery_name)));
+        timeDecrementBtn.setOnClickListener(this);
+        timeIncrementBtn.setOnClickListener(this);
+        weatherDecrementBtn.setOnClickListener(this);
+        weatherIncrementBtn.setOnClickListener(this);
 
-            // notification preference change listener
-            bindPreferenceSummaryToValue(findPreference(getString(R.string.key_notifications_new_message_ringtone)));
 
-            // feedback preference click listener
-            Preference myPref = findPreference(getString(R.string.key_send_feedback));
-            myPref.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
-                public boolean onPreferenceClick(Preference preference) {
-                    sendFeedback(getActivity());
-                    return true;
+        final Switch time_module_enable = findViewById(R.id.time_module_enable);
+        final Switch weather_module_enable = findViewById(R.id.weather_module_enable);
+        Switch hour24_switch = findViewById(R.id.hour24_switch);
+        Switch celcius_switch = findViewById(R.id.celcius_switch);
+
+        CompoundButton.OnCheckedChangeListener multiListener = new CompoundButton.OnCheckedChangeListener() {
+
+            public void onCheckedChanged(CompoundButton v, boolean isChecked) {
+                switch (v.getId()){
+                    case R.id.time_module_enable:
+                        LinearLayout layout1 = (LinearLayout) findViewById(R.id.timeModuleLayout);
+                        if(isChecked){
+                            enable(layout1);
+                        }else{
+                            disable(layout1);
+                            time_module_enable.setEnabled(true);
+                        }
+                        break;
+
+                    case R.id.weather_module_enable:
+                        LinearLayout layout2 = (LinearLayout) findViewById(R.id.weatherModuleLayout);
+                        if(isChecked){
+                            enable(layout2);
+                        }else{
+                            disable(layout2);
+                            weather_module_enable.setEnabled(true);
+                        }
+                        break;
+                    case R.id.hour24_switch:
+                        if(isChecked){
+                           //TODO
+                        }else{
+                            //TODO
+                        }
+
+                        break;
+                    case R.id.celcius_switch:
+                        if(isChecked){
+                            //TODO
+                        }else{
+                            //TODO
+                        }
+                        break;
                 }
-            });
-        }
+            }
+        };
+
+
+        time_module_enable.setOnCheckedChangeListener(multiListener);
+        weather_module_enable.setOnCheckedChangeListener(multiListener);
+        hour24_switch.setOnCheckedChangeListener(multiListener);
+        celcius_switch.setOnCheckedChangeListener(multiListener);
+
+
+
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        if (item.getItemId() == android.R.id.home) {
-            onBackPressed();
+    public void onClick(View v) {
+        TextView txtSizeView;
+        TextView txtSize;
+        switch (v.getId()){
+
+            case R.id.timeDecrementBtn:
+                txtSizeView = (TextView) findViewById(R.id.timeModule_textSize_view);
+                txtSize = (TextView) findViewById(R.id.timeModule_textSizeInt);
+                decrement(txtSizeView,txtSize);
+
+                break;
+            case R.id.timeIncrementBtn:
+                txtSizeView = (TextView) findViewById(R.id.timeModule_textSize_view);
+                txtSize = (TextView) findViewById(R.id.timeModule_textSizeInt);
+                increment(txtSizeView,txtSize);
+                break;
+            case R.id.weatherDecrementBtn:
+                txtSizeView = (TextView) findViewById(R.id.weatherModule_textSize_view);
+                txtSize = (TextView) findViewById(R.id.weatherModule_textSizeInt);
+                decrement(txtSizeView,txtSize);
+                break;
+            case R.id.weatherIncrementBtn:
+
+                txtSizeView = (TextView) findViewById(R.id.weatherModule_textSize_view);
+                txtSize = (TextView) findViewById(R.id.weatherModule_textSizeInt);
+                increment(txtSizeView,txtSize);
+
+                break;
+
+
         }
-        return super.onOptionsItemSelected(item);
+
+    }
+    public void decrement(TextView txtSizeView,TextView txtSize) {
+
+        int currentSize = Integer.parseInt(txtSizeView.getText().toString());
+        txtSizeView.setText(""+(Math.max(0,currentSize-1)));
+        txtSize.setTextSize(currentSize-1);
+
     }
 
-    private static void bindPreferenceSummaryToValue(Preference preference) {
-        preference.setOnPreferenceChangeListener(sBindPreferenceSummaryToValueListener);
-
-        sBindPreferenceSummaryToValueListener.onPreferenceChange(preference,
-                PreferenceManager
-                        .getDefaultSharedPreferences(preference.getContext())
-                        .getString(preference.getKey(), ""));
+    public void increment(TextView txtSizeView,TextView txtSize) {
+        int currentSize = Integer.parseInt(txtSizeView.getText().toString());
+        txtSizeView.setText(""+(Math.max(0,currentSize+1)));
+        txtSize.setTextSize(currentSize+1);
     }
 
-    /**
-     * A preference value change listener that updates the preference's summary
-     * to reflect its new value.
-     */
-    private static Preference.OnPreferenceChangeListener sBindPreferenceSummaryToValueListener = new Preference.OnPreferenceChangeListener() {
-        @Override
-        public boolean onPreferenceChange(Preference preference, Object newValue) {
-            String stringValue = newValue.toString();
-
-            if (preference instanceof ListPreference) {
-                // For list preferences, look up the correct display value in
-                // the preference's 'entries' list.
-                ListPreference listPreference = (ListPreference) preference;
-                int index = listPreference.findIndexOfValue(stringValue);
-
-                // Set the summary to reflect the new value.
-                preference.setSummary(
-                        index >= 0
-                                ? listPreference.getEntries()[index]
-                                : null);
-
-            } else if (preference instanceof RingtonePreference) {
-                // For ringtone preferences, look up the correct display value
-                // using RingtoneManager.
-                if (TextUtils.isEmpty(stringValue)) {
-                    // Empty values correspond to 'silent' (no ringtone).
-                    preference.setSummary(R.string.pref_ringtone_silent);
-
-                } else {
-                    Ringtone ringtone = RingtoneManager.getRingtone(
-                            preference.getContext(), Uri.parse(stringValue));
-
-                    if (ringtone == null) {
-                        // Clear the summary if there was a lookup error.
-                        preference.setSummary(R.string.summary_choose_ringtone);
-                    } else {
-                        // Set the summary to reflect the new ringtone display
-                        // name.
-                        String name = ringtone.getTitle(preference.getContext());
-                        preference.setSummary(name);
-                    }
-                }
-
-            } else if (preference instanceof EditTextPreference) {
-                if (preference.getKey().equals("key_gallery_name")) {
-                    // update the changed gallery name to summary filed
-                    preference.setSummary(stringValue);
-                }
+    private static void enable(ViewGroup layout) {
+        layout.setEnabled(false);
+        for (int i = 0; i < layout.getChildCount(); i++) {
+            View child = layout.getChildAt(i);
+            if (child instanceof ViewGroup) {
+                enable((ViewGroup) child);
             } else {
-                preference.setSummary(stringValue);
+                child.setEnabled(true);
             }
-            return true;
         }
-    };
+    }
+    private static void disable(ViewGroup layout) {
+        layout.setEnabled(false);
+        for (int i = 0; i < layout.getChildCount(); i++) {
+            View child = layout.getChildAt(i);
+            if (child instanceof ViewGroup) {
+                disable((ViewGroup) child);
+            } else {
+                child.setEnabled(false);
+            }
+        }
 
-    /**
-     * Email client intent to send support mail
-     * Appends the necessary device information to email body
-     * useful when providing support
-     */
-    public static void sendFeedback(Context context) {
-        String body = null;
-        try {
-            body = context.getPackageManager().getPackageInfo(context.getPackageName(), 0).versionName;
-            body = "\n\n-----------------------------\nPlease don't remove this information\n Device OS: Android \n Device OS version: " +
-                    Build.VERSION.RELEASE + "\n App Version: " + body + "\n Device Brand: " + Build.BRAND +
-                    "\n Device Model: " + Build.MODEL + "\n Device Manufacturer: " + Build.MANUFACTURER;
-        } catch (PackageManager.NameNotFoundException e) {
-        }
-        Intent intent = new Intent(Intent.ACTION_SEND);
-        intent.setType("message/rfc822");
-        intent.putExtra(Intent.EXTRA_EMAIL, new String[]{"contact@androidhive.info"});
-        intent.putExtra(Intent.EXTRA_SUBJECT, "Query from android app");
-        intent.putExtra(Intent.EXTRA_TEXT, body);
-        context.startActivity(Intent.createChooser(intent, context.getString(R.string.choose_email_client)));
     }
 
-    public void onBackPressed(){
-        Intent backToMain = new Intent(SettingsActivity.this,MainActivity.class);
-        finish();
-        startActivity(backToMain);
-    }
+
 }
