db6f29d16dcfd825797dbe8752b400b42c1eb834
Enes Malik Turhan <emalikturhan@gmail.com>
2019-01-27 22:59:54 +0300

    settings ekranına time ve weather modulleri eklendi

--- phoneAndroidApp/app/src/main/java/com/example/panta/orangemirror/MainActivity.java
+++ phoneAndroidApp/app/src/main/java/com/example/panta/orangemirror/MainActivity.java
@@ -4,27 +4,35 @@ import android.content.Intent;
 import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 import android.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
 
-
+    EditText name;
+    Editable username;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+         name = (EditText) findViewById(R.id.editText);
+        username = name.getText();
+
 
         Button button = (Button) findViewById(R.id.button);
         button.setOnClickListener(new View.OnClickListener() {
             public void onClick(View view) {
-              //  startActivity(new Intent(MainActivity.this, SettingsActivity.class));
+
                 Intent mainIntent = new Intent(MainActivity.this,SettingsActivity.class);
-                MainActivity.this.startActivity(mainIntent);
-                MainActivity.this.finish();
+               mainIntent.putExtra("USERNAME", username.toString());
+               MainActivity.this.startActivity(mainIntent);
+               MainActivity.this.finish();
             }
         });
     }
