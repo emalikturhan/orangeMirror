94c88e3d3e80a2beb94c2f621e85b9191d5888a6
Enes Malik Turhan <emalikturhan@gmail.com>
2019-01-29 20:52:25 +0300

    Revert "settings ekranına time ve weather modulleri eklendi"
    This reverts commit f93fa2568716e000998ca82df20748490bd54ec4, reversing
    changes made to db6f29d16dcfd825797dbe8752b400b42c1eb834.

--- WebApiServer/db.py
+++ WebApiServer/db.py
@@ -18,63 +18,33 @@ def save_db(data):
         f.close()
 
 
-def create_person(user_name, home):
-    if(check_name_exist(user_name)):
+def create_person(name, home):
+    if(check_name_exist(name)):
         print("this name exists.")
-        return False
+        return
     data = get_db()
     # get person_1 VALUE
     default_person = json.loads(str(data["person_1"]).replace("'", "\""))
     person = default_person
     # PersonGroup Person Create
-    personId = create_face_api_personGroup_person(user_name)
-    # Person id person idye kayit edilecek
+    personId = create_face_api_personGroup_person(name)
+    # Person id person idye kayıt edilecek
     person["personId"] = personId
-    person["user_name"] = user_name
+    person["name"] = name
     person["home"] = home
     person_number = len(data.keys()) + 1
     data["person_"+str(person_number)] = person
     print(data)
     save_db(data)
-    return True
 
 
-def update_settings_for_user(name, settings_json):
-    user_json = {}
-    data = get_db()
-    person_id = ""
-    for d in data:
-        if(data[d]["user_name"].lower() == name.lower()):
-            user_json = data[d]
-            print("user_json"+str(user_json))
-            person_id = d
-            break
-    data[person_id]["settings"] = settings_json
-    save_db(data)
-    print("UPDATED"+str(data[person_id]))
-    return data[person_id]
-
-
-def get_settings_for_user(user_name):
-    user_json = {}
-    data = get_db()
-    person_id = ""
-    for d in data:
-        if(data[d]["user_name"].lower() == user_name.lower()):
-            user_json = data[d]
-            print("user_json"+str(user_json))
-            person_id = d
-            break
-    return data[person_id]
-
-
-def create_face_api_personGroup_person(user_name):
+def create_face_api_personGroup_person(name):
     url = "https://westeurope.api.cognitive.microsoft.com/face/v1.0/persongroups/4567/persons"
     headers = {
         "content-type": "application/json",
         "Ocp-Apim-Subscription-Key": "c5e2d62a223946ca9e3f35b3fef75cef"
     }
-    payload = "{\"name\":\"" + user_name + "\"}"
+    payload = "{\"name\":\"" + name + "\"}"
     print(str(payload))
     r = requests.post(url=url, data=payload, headers=headers)
     print(r.content)
@@ -94,9 +64,9 @@ def train_face_api_personGroup_person():
     r = requests.post(url=url, headers=headers)
 
 
-def add_face_face_api_personGroup_person(user_name, face_url):
+def add_face_face_api_personGroup_person(name, face_url):
     url = "https: // westeurope.api.cognitive.microsoft.com/face/v1.0/persongroups/4567/persons/" + \
-        user_name+"/persistedFaces"
+        name+"/persistedFaces"
     payload = "{\"url\":\"" + face_url + "\"}"
     headers = {
         "content-type": "application/json",
@@ -107,7 +77,7 @@ def add_face_face_api_personGroup_person(user_name, face_url):
 
 def detect_face_api(face_url):
     url = "https://westeurope.api.cognitive.microsoft.com/face/v1.0/detect?returnFaceId=true"
-    # payload = "{\"url\":\"" + face_url + "\"}"
+    #payload = "{\"url\":\"" + face_url + "\"}"
     payload = json.dumps({"url": face_url})
     headers = {
         "content-type": "application/json",
@@ -143,11 +113,11 @@ def identify_face_api(face_url):
         response = json.loads(r.text)
         personId = response[0]["candidates"][0]["personId"]
         confidence = float(response[0]["candidates"][0]["confidence"])
-        # belli threshold ustunde ise addFace yap
+        # belli threshold üstünde ise addFace yap
         print(confidence)
         print(personId)
-        user_name = get_name(personId)
-        return user_name
+        name = get_name(personId)
+        return name
     except:
         return "nullFaceId"
     print("a")
@@ -159,43 +129,24 @@ def get_name(personId):
 
         d_personId = data[d]["personId"]
         if(d_personId == personId):
-            user_name = data[d]["name"]
-            return user_name
+            name = data[d]["name"]
+            return name
     return "nullName"
 
 
-def check_name_exist(user_name):
+def check_name_exist(name):
     data = get_db()
     for d in data:
-        if(data[d]["user_name"].lower() == user_name.lower()):
-            print("DB"+data[d]["user_name"].lower())
-            print("user_name"+user_name.lower())
+        if(data[d]["name"].lower() == name.lower()):
             return True
-    return False  # False
+    return True  # False
 
 
 # train_face_api_personGroup_person()
-# create_person("abc", "mustafa_home")
+#create_person("abc", "mustafa_home")
 print(identify_face_api("https://i.postimg.cc/Z585whdX/web.jpg"))
-settings_json = {
-    "user_name": "enez",
-    "settings": [
-        {
-            "time_module_enable": "true",
-            "timeTxtSize": "210.0",
-            "hour24_enable": "false"
-        },
-        {
-            "weather_module": "true",
-            "weatherTxtSize": "300",
-            "celcius_enable": "false"
-        }
-    ]
-}
-
-# update_settings_for_user("enez", settings_json)
-#data = get_db()
-# print(data)
+data = get_db()
+print(data)
 
 """
 data["person_1"]["home"] = "home1"
