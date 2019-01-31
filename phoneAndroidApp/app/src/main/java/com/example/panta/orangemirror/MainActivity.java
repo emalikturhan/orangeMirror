007a83dfcea00ca60bfeed9372ba8700db86ea5a
Enes Malik Turhan <emalikturhan@gmail.com>
2019-01-31 21:56:34 +0300

    Settings'e date modulu eklendi

--- phoneAndroidApp/app/src/main/java/com/example/panta/orangemirror/MainActivity.java
+++ phoneAndroidApp/app/src/main/java/com/example/panta/orangemirror/MainActivity.java
@@ -4,33 +4,38 @@ import android.content.Intent;
 import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toolbar;
+
 
 public class MainActivity extends AppCompatActivity {
 
+    EditText name;
+    Editable username;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        name = (EditText) findViewById(R.id.editText);
+        username = name.getText();
+
 
         Button button = (Button) findViewById(R.id.button);
         button.setOnClickListener(new View.OnClickListener() {
             public void onClick(View view) {
-              //  startActivity(new Intent(MainActivity.this, SettingsActivity.class));
-                Intent mainIntent = new Intent(MainActivity.this,SettingsActivity.class);
+
+                //  startActivity(new Intent(MainActivity.this, SettingsActivity.class));
+                Intent mainIntent = new Intent(MainActivity.this, SettingsActivity.class);
+                mainIntent.putExtra("USERNAME", username.toString());
                 MainActivity.this.startActivity(mainIntent);
                 MainActivity.this.finish();
             }
         });
     }
-    public void onBackPressed(){
-        this.finish();
-    }
-
-
 }
--- phoneAndroidApp/app/src/main/java/com/example/panta/orangemirror/SettingsActivity.java
+++ phoneAndroidApp/app/src/main/java/com/example/panta/orangemirror/SettingsActivity.java
@@ -1,6 +1,7 @@
 package com.example.panta.orangemirror;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,12 +12,40 @@ import android.widget.Switch;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 public class SettingsActivity extends Activity implements View.OnClickListener{
+
+
+    private boolean time_module_enable_bool, weather_module_enable_bool,date_module_enable_bool;
+    private String username, timeTxtSize, weatherTxtSize,dateTxtSize;
+    private boolean hour24_enable,celcius_enable;
+    JSONObject settings;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+
+        Intent intent = getIntent();
+
+
+        //////////INITIALIZE SETTINGS VARIABLES /////////////
+        username = intent.getStringExtra("USERNAME");
+        time_module_enable_bool = true;
+        weather_module_enable_bool= true;
+        date_module_enable_bool=true;
+
+        timeTxtSize= ((TextView) findViewById(R.id.timeModule_textSizeInt)).getTextSize() +"";
+        weatherTxtSize= ((TextView) findViewById(R.id.weatherModule_textSizeInt)).getTextSize() +"";
+        dateTxtSize= ((TextView) findViewById(R.id.weatherModule_textSizeInt)).getTextSize() +"";
+
+        hour24_enable=true;
+        celcius_enable=true;
+
+        /////////////////////////////////////////////////////
 
         Button timeDecrementBtn = (Button) findViewById(R.id.timeDecrementBtn);
         Button timeIncrementBtn = (Button) findViewById(R.id.timeIncrementBtn);
@@ -24,14 +53,24 @@ public class SettingsActivity extends Activity implements View.OnClickListener{
         Button weatherDecrementBtn = (Button) findViewById(R.id.weatherDecrementBtn);
         Button weatherIncrementBtn = (Button) findViewById(R.id.weatherIncrementBtn);
 
+        Button dateDecrementBtn = (Button) findViewById(R.id.dateDecrementBtn);
+        Button dateIncrementBtn = (Button) findViewById(R.id.dateIncrementBtn);
+
+
+        Button saveBtn = (Button) findViewById(R.id.saveBtn);
+
         timeDecrementBtn.setOnClickListener(this);
         timeIncrementBtn.setOnClickListener(this);
         weatherDecrementBtn.setOnClickListener(this);
         weatherIncrementBtn.setOnClickListener(this);
-
+        dateDecrementBtn.setOnClickListener(this);
+        dateIncrementBtn.setOnClickListener(this);
+        saveBtn.setOnClickListener(this);
 
         final Switch time_module_enable = findViewById(R.id.time_module_enable);
         final Switch weather_module_enable = findViewById(R.id.weather_module_enable);
+        final Switch date_module_enable = findViewById(R.id.date_module_enable);
+
         Switch hour24_switch = findViewById(R.id.hour24_switch);
         Switch celcius_switch = findViewById(R.id.celcius_switch);
 
@@ -43,9 +82,11 @@ public class SettingsActivity extends Activity implements View.OnClickListener{
                         LinearLayout layout1 = (LinearLayout) findViewById(R.id.timeModuleLayout);
                         if(isChecked){
                             enable(layout1);
+                            time_module_enable_bool=true;
                         }else{
                             disable(layout1);
                             time_module_enable.setEnabled(true);
+                            time_module_enable_bool=false;
                         }
                         break;
 
@@ -53,24 +94,37 @@ public class SettingsActivity extends Activity implements View.OnClickListener{
                         LinearLayout layout2 = (LinearLayout) findViewById(R.id.weatherModuleLayout);
                         if(isChecked){
                             enable(layout2);
+                            weather_module_enable_bool= true;
                         }else{
                             disable(layout2);
                             weather_module_enable.setEnabled(true);
+                            weather_module_enable_bool= false;
+                        }
+                        break;
+                    case R.id.date_module_enable:
+                        LinearLayout layout3 = (LinearLayout) findViewById(R.id.dateModuleLayout);
+                        if(isChecked){
+                            enable(layout3);
+                            date_module_enable_bool= true;
+                        }else{
+                            disable(layout3);
+                            date_module_enable.setEnabled(true);
+                            date_module_enable_bool= false;
                         }
                         break;
                     case R.id.hour24_switch:
                         if(isChecked){
-                           //TODO
+                            hour24_enable=true;
                         }else{
-                            //TODO
+                            hour24_enable=false;
                         }
 
                         break;
                     case R.id.celcius_switch:
                         if(isChecked){
-                            //TODO
+                            celcius_enable=true;
                         }else{
-                            //TODO
+                            celcius_enable=false;
                         }
                         break;
                 }
@@ -80,9 +134,10 @@ public class SettingsActivity extends Activity implements View.OnClickListener{
 
         time_module_enable.setOnCheckedChangeListener(multiListener);
         weather_module_enable.setOnCheckedChangeListener(multiListener);
+        date_module_enable.setOnCheckedChangeListener(multiListener);
+
         hour24_switch.setOnCheckedChangeListener(multiListener);
         celcius_switch.setOnCheckedChangeListener(multiListener);
-
 
 
     }
@@ -114,6 +169,40 @@ public class SettingsActivity extends Activity implements View.OnClickListener{
                 txtSizeView = (TextView) findViewById(R.id.weatherModule_textSize_view);
                 txtSize = (TextView) findViewById(R.id.weatherModule_textSizeInt);
                 increment(txtSizeView,txtSize);
+
+                break;
+
+            case R.id.dateDecrementBtn:
+                txtSizeView = (TextView) findViewById(R.id.dateModule_textSize_view);
+                txtSize = (TextView) findViewById(R.id.dateModule_textSizeInt);
+                decrement(txtSizeView,txtSize);
+                break;
+            case R.id.dateIncrementBtn:
+
+                txtSizeView = (TextView) findViewById(R.id.dateModule_textSize_view);
+                txtSize = (TextView) findViewById(R.id.dateModule_textSizeInt);
+                increment(txtSizeView,txtSize);
+
+                break;
+
+            case R.id.saveBtn:
+                settings=createJSON();
+
+                JSONObject deneme = new JSONObject();
+                try {
+                    deneme.put("user_name","deneme");
+                    deneme.put("home","deneme");
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+                new SendDeviceDetails().execute("http://24.133.185.104:5000/register", deneme.toString());
+
+
+
+                String jsonStr = settings.toString();
+                System.out.println("jsonString: "+jsonStr);
+                Toast.makeText(getApplicationContext(), jsonStr, Toast.LENGTH_LONG).show();
 
                 break;
 
@@ -157,6 +246,58 @@ public class SettingsActivity extends Activity implements View.OnClickListener{
             }
         }
 
+    }
+    private JSONObject createJSON(){
+
+        ////////////////////////CREATING JSON OBJECT///////////////////////////////
+        JSONObject timeModuleObj = new JSONObject();
+        try {
+
+            timeModuleObj.put("time_module_enable", time_module_enable_bool);
+            timeModuleObj.put("timeTxtSize", timeTxtSize);
+            timeModuleObj.put("hour24_enable", hour24_enable);
+
+        } catch (JSONException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
+        JSONObject weatherModuleObj = new JSONObject();
+        try {
+            weatherModuleObj.put("weather_module_enable", weather_module_enable_bool);
+            weatherModuleObj.put("weatherTxtSize", weatherTxtSize);
+            weatherModuleObj.put("celcius_enable", celcius_enable);
+
+        } catch (JSONException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
+        JSONObject dateModuleObj = new JSONObject();
+        try {
+            dateModuleObj.put("date_module_enable", date_module_enable_bool);
+            dateModuleObj.put("dateTxtSize", dateTxtSize);
+
+
+        } catch (JSONException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
+        JSONArray jsonArray = new JSONArray();
+
+        jsonArray.put(timeModuleObj);
+        jsonArray.put(weatherModuleObj);
+        jsonArray.put(dateModuleObj);
+
+        settings = new JSONObject();
+        try {
+            settings.put("username", username);
+            settings.put("Settings", jsonArray);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return settings;
     }
 
 
