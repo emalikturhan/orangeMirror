3c122d0ebb0c941bed0b95552cb5cb0c47f0f076
abozgungordu <abozgungordu@gmail.com>
2019-03-22 17:05:49 +0300

    GOOGLE CALENDAR API telefona entegre edildi

---
+++ phoneAndroidApp/app/src/main/java/com/example/panta/orangemirror/CalendarActivity.java
@@ -0,0 +1,262 @@
+package com.example.panta.orangemirror;
+
+
+
+
+import android.os.Bundle;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.api.client.extensions.android.http.AndroidHttp;
+import com.google.api.client.googleapis.extensions.android.gms.auth.GoogleAccountCredential;
+import com.google.android.gms.common.GooglePlayServicesUtil;
+import com.google.api.client.json.JsonFactory;
+import com.google.api.client.json.gson.GsonFactory;
+import com.google.api.client.http.HttpTransport;
+import com.google.api.client.util.ExponentialBackOff;
+import com.google.api.services.calendar.CalendarScopes;
+import android.accounts.AccountManager;
+import android.app.Dialog;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.graphics.Typeface;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.text.TextUtils;
+import android.text.method.ScrollingMovementMethod;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.app.Activity;
+import java.util.Arrays;
+import java.util.List;
+
+public class CalendarActivity extends Activity {
+
+
+    // Google Calendar API servis objeleri kullanılarak API'ye erişildi
+
+
+
+    com.google.api.services.calendar.Calendar mService;
+    GoogleAccountCredential credential;
+    private TextView mStatusText;
+    private TextView mResultsText;
+    final HttpTransport transport = AndroidHttp.newCompatibleTransport();
+    final JsonFactory jsonFactory = GsonFactory.getDefaultInstance();
+
+    static final int REQUEST_ACCOUNT_PICKER = 1000;
+    static final int REQUEST_AUTHORIZATION = 1001;
+    static final int REQUEST_GOOGLE_PLAY_SERVICES = 1002;
+    private static final String PREF_ACCOUNT_NAME = "hesapAdi";
+    private static final String[] SCOPES = { CalendarScopes.CALENDAR_READONLY };
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        LinearLayout activityLayout = new LinearLayout(this);
+        LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.MATCH_PARENT,
+                LinearLayout.LayoutParams.MATCH_PARENT);
+        activityLayout.setLayoutParams(lp);
+        activityLayout.setOrientation(LinearLayout.VERTICAL);
+        activityLayout.setPadding(16, 16, 16, 16);
+
+        ViewGroup.LayoutParams tlp = new ViewGroup.LayoutParams(
+                ViewGroup.LayoutParams.WRAP_CONTENT,
+                ViewGroup.LayoutParams.WRAP_CONTENT);
+
+        mStatusText = new TextView(this);
+        mStatusText.setLayoutParams(tlp);
+        mStatusText.setTypeface(null, Typeface.BOLD);
+        mStatusText.setText("Veriler yükleniyor...");
+        activityLayout.addView(mStatusText);
+
+        mResultsText = new TextView(this);
+        mResultsText.setLayoutParams(tlp);
+        mResultsText.setPadding(16, 16, 16, 16);
+        mResultsText.setVerticalScrollBarEnabled(true);
+        mResultsText.setMovementMethod(new ScrollingMovementMethod());
+        activityLayout.addView(mResultsText);
+
+        setContentView(activityLayout);
+
+
+        SharedPreferences settings = getPreferences(Context.MODE_PRIVATE);
+        credential = GoogleAccountCredential.usingOAuth2(
+                getApplicationContext(), Arrays.asList(SCOPES))
+                .setBackOff(new ExponentialBackOff())
+                .setSelectedAccountName(settings.getString(PREF_ACCOUNT_NAME, null));
+
+        mService = new com.google.api.services.calendar.Calendar.Builder(
+                transport, jsonFactory, credential)
+                .setApplicationName("mirrorAndroidApp")
+                .build();
+    }
+
+
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (isGooglePlayServicesAvailable()) {
+            refreshResults();
+        } else {
+            mStatusText.setText("Google Play Services : " +
+                    "yükledikten sonra, uygulamayı kapatın ve yeniden başlatın.");
+        }
+    }
+
+
+    /**
+     * Burada bir activity çalıştığın ortaya çıkar, size başladığınız requestCode'u verir.
+     * Buna göre bir resultCode ve ek verileri döndürür.
+     * @param requestCode hangi aktivitenin başlatıldığının kodunu gösterir.
+     * @param resultCode gelen aktivitenin sonucununun kodunu döner.
+     * @param data gelen aktivitenin verileridir.
+     */
+
+    @Override
+    protected void onActivityResult(
+            int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        switch(requestCode) {
+            case REQUEST_GOOGLE_PLAY_SERVICES:
+                if (resultCode == RESULT_OK) {
+                    refreshResults();
+                } else {
+                    isGooglePlayServicesAvailable();
+                }
+                break;
+            case REQUEST_ACCOUNT_PICKER:
+                if (resultCode == RESULT_OK && data != null &&
+                        data.getExtras() != null) {
+                    String accountName =
+                            data.getStringExtra(AccountManager.KEY_ACCOUNT_NAME);
+                    if (accountName != null) {
+                        credential.setSelectedAccountName(accountName);
+                        SharedPreferences settings =
+                                getPreferences(Context.MODE_PRIVATE);
+                        SharedPreferences.Editor editor = settings.edit();
+                        editor.putString(PREF_ACCOUNT_NAME, accountName);
+                        editor.commit();
+                        refreshResults();
+                    }
+                } else if (resultCode == RESULT_CANCELED) {
+                    mStatusText.setText("Hesap bulunamadı.");
+                }
+                break;
+            case REQUEST_AUTHORIZATION:
+                if (resultCode == RESULT_OK) {
+                    refreshResults();
+                } else {
+                    chooseAccount();
+                }
+                break;
+        }
+
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+
+    // Kayıtlı hesabın olup olmadığını kontrol eder eğer yoksa hesap seçmenizi ister.
+
+    private void refreshResults() {
+        if (credential.getSelectedAccountName() == null) {
+            chooseAccount();
+        } else {
+            if (isDeviceOnline()) {
+                new ApiAsyncTask(this).execute();
+            } else {
+                mStatusText.setText("Aktif bir internet bağlantısı yok.");
+            }
+        }
+    }
+
+
+    public void clearResultsText() {
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                mStatusText.setText("Veriler yükleniyor...");
+                mResultsText.setText("");
+            }
+        });
+    }
+
+
+    public void updateResultsText(final List<String> dataStrings) {
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                if (dataStrings == null) {
+                    mStatusText.setText("Veriler yüklenemedi..!");
+                } else if (dataStrings.size() == 0) {
+                    mStatusText.setText("Veri bulunamadı.");
+                } else {
+                    mStatusText.setText("Google Calendar API kullanarak veriler yükleniyor...");
+                    mResultsText.setText(TextUtils.join("\n\n", dataStrings));
+                }
+            }
+        });
+    }
+
+
+
+
+    public void updateStatus(final String message) {
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                mStatusText.setText(message);
+            }
+        });
+    }
+
+
+    // Hesap seçmek için yeni bir aktivite oluşturur.
+
+    private void chooseAccount() {
+        startActivityForResult(
+                credential.newChooseAccountIntent(), REQUEST_ACCOUNT_PICKER);
+    }
+
+
+
+    private boolean isDeviceOnline() {
+        ConnectivityManager connMgr =
+                (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
+        return (networkInfo != null && networkInfo.isConnected());
+    }
+
+
+    private boolean isGooglePlayServicesAvailable() {
+        final int connectionStatusCode =
+                GooglePlayServicesUtil.isGooglePlayServicesAvailable(this);
+        if (GooglePlayServicesUtil.isUserRecoverableError(connectionStatusCode)) {
+            showGooglePlayServicesAvailabilityErrorDialog(connectionStatusCode);
+            return false;
+        } else if (connectionStatusCode != ConnectionResult.SUCCESS ) {
+            return false;
+        }
+        return true;
+    }
+
+
+    void showGooglePlayServicesAvailabilityErrorDialog(
+            final int connectionStatusCode) {
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                Dialog dialog = GooglePlayServicesUtil.getErrorDialog(
+                        connectionStatusCode,
+                        CalendarActivity.this,
+                        REQUEST_GOOGLE_PLAY_SERVICES);
+                dialog.show();
+            }
+        });
+    }
+
+}
