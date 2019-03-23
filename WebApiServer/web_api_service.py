5f7ff829962b391d4ffb93572079e851f947a2f5
mustafatokmak <mtokmak06@gmail.com>
2019-03-23 15:18:31 +0300

    .

--- WebApiServer/web_api_service.py
+++ WebApiServer/web_api_service.py
@@ -102,7 +102,42 @@ def upload_file():
     return json.dumps(response)
 
 
-#def register_
+@app.route('/api/register_photo', methods=['POST', 'GET'])
+def register_photo():
+    start = time.time()
+    user_name = "enezzzzzzzzz"
+    if request.method == 'POST':
+        print(request.get_json())
+        files = request.files
+        print("start")
+        for f in files:
+            file = request.files[f]
+            counter = random.randint(1, 101)
+            filename = "photo_"+str(counter)+".jpg"
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+
+            if(True):
+                filename = upload_folder + "/" + filename
+                print(filename)
+                db.add_face_face_api_personGroup_person_binary(filename)
+                identified_user, score = db.identify_face_api_with_binary(
+                    filename)
+                print(db.identify_face_api_with_binary(filename))
+
+            if(len(os.listdir(upload_folder)) > 5 and False):
+                file_list = os.listdir(upload_folder)
+                for fi in file_list:
+                    filename = upload_folder + "/" + fi
+                    db.add_face_face_api_personGroup_person_binary(
+                        user_name, filename)
+                db.train_face_api_personGroup_person()
+                #messages[model_created] = True
+    response = {}
+    response["username"] = identified_user
+    response["score"] = score
+    finish = time.time()
+    print(finish-start)
+    return json.dumps(response)
 
 
 @app.route('/api/events', methods=['POST', 'GET'])
